93bb2d1d5b1dc552366558734d9efef09efd3652
John Cañero <johncanero.official@gmail.com>
2023-08-23 18:02:58 +0800

    Add SignUp and Login Buttons at the Top Right of Header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className='bg-neutral-900 rounded-lg h-full overflow-hidden overflow-y-auto md:mr-2'>
       <Header>
-       
+          Header
       </Header>
     </div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
 
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+
+import Button from './Button';
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -24,6 +29,7 @@ const Header = ({ children, className }: HeaderProps) => {
             )}
         >
             <div className='w-full mb-4 flex items-center justify-between'>
+                {/* Buttons */}
                 <div className='hidden md:flex gap-x-2 items-center'>
                     {/* RxCaretLeft */}
                     <button
@@ -41,7 +47,46 @@ const Header = ({ children, className }: HeaderProps) => {
                         <RxCaretRight size={35} className='text-white' />
                     </button>
                 </div>
+
+                {/* Home and Search Button */}
+                <div className='flex md:hidden gap-x-2 items-center'>
+                    <Link href={'/'}>
+                        <button
+                            className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'
+                        >
+                            <HiHome className='text-black' size={20} />
+                        </button>
+                    </Link>
+                    <Link href={'/search'}>
+                        <button
+                            className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'
+                        >
+                            <BiSearch className='text-black' size={20} />
+                        </button>
+                    </Link>
+                </div>
+
+                <div className='flex justify-between items-center gap-x-4'>
+                    <>
+                        <div>
+                            <Button
+                                onClick={() => { }}
+                                className='bg-transparent text-neutral-300 font-medium'>
+                                Sign Up
+                            </Button>
+                        </div>
+
+                        <div>
+                            <Button
+                                onClick={() => { }}
+                                className='bg-white px-6 py-2'>
+                                Log In
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {children}
         </div>
     );
 }
