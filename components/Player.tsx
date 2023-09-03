bb66989892a9b872bd25c2e602f5d44c6c4253a8
John Cañero <johncanero.official@gmail.com>
2023-09-03 14:48:27 +0800

    Add Player Content MediaItem and LikedButton

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,22 +4,28 @@ import usePlayer from "@/hooks/usePlayer";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import useGetSongById from "@/hooks/useGetSongById";
 
+import PlayerContent from "./PlayerContent";
+
 const Player = () => {
     const player = usePlayer();
     const { song } = useGetSongById(player.activeId);
-    
+
     const songUrl = useLoadSongUrl(song!);
-    
+
     if (!song || !songUrl || !player.activeId) {
         return null;
     }
 
-    return ( 
+    return (
         <div
             className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-            Player
-        </div> 
-     );
+            <PlayerContent
+                key={songUrl}
+                song={song}
+                songUrl={songUrl}
+            />
+        </div>
+    );
 }
- 
+
 export default Player;
